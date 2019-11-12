d1217d2a54b4a6578540c870123c6410d1fd9ff8
JP Navarro <jpfnavarro@gmail.com>
2019-11-12 20:48:59 +0000

    Fix incorrect sbin files names

--- sbin/route_monitoring.sh
+++ sbin/route_monitoring.sh
@@ -1,18 +1,49 @@
-[Unit]
-Description=Information Services Monitoring Router 
-After=network.target remote-fs.target nss-lookup.target
+#!/bin/sh
+do_start () {
+    echo -n "Starting ${DAEMON_NAME}:"
+    export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+    source ${PIPENV_BASE}/bin/activate
+    ${PIPENV_BASE}/bin/python ${DAEMON_BIN} start ${DAEMON_OPTS}
+    RETVAL=$?
+}
+do_stop () {
+    echo -n "Stopping ${DAEMON_NAME}:"
+    export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+    source ${PIPENV_BASE}/bin/activate
+    ${PIPENV_BASE}/bin/python ${DAEMON_BIN} stop ${DAEMON_OPTS}
+    RETVAL=$?
+}
+do_debug () {
+    echo -n "Debugging: ${PIPENV_BASE}/bin/python ${DAEMON_BIN} $@ ${DAEMON_OPTS}"
+    export LD_LIBRARY_PATH=${PYTHON_BASE}/lib
+    source ${PIPENV_BASE}/bin/activate
+    ${PIPENV_BASE}/bin/python ${DAEMON_BIN} $@ ${DAEMON_OPTS}
+    RETVAL=$?
+}
 
-[Service]
-Type=forking
-EnvironmentFile=/soft/warehouse-apps-1.0/Manage-Monitoring/PROD/sbin/route_monitoring.sysconfig
-ExecStart=/soft/warehouse-apps-1.0/Manage-Monitoring/PROD/sbin/route_monitoring.sh start
-ExecReload=/soft/warehouse-apps-1.0/Manage-Monitoring/PROD/sbin/route_monitoring.sh restart
-ExecStop=/soft/warehouse-apps-1.0/Manage-Monitoring/PROD/sbin/route_monitoring.sh stop
-PIDFile=/soft/warehouse-apps-1.0/Manage-Monitoring/var/route_monitoring.pid
-#PrivateTmp=true
-User=software
-Group=admin
-Restart=always
+case "$1" in
+    start|stop)
+        do_${1}
+        ;;
 
-[Install]
-WantedBy=multi-user.target
+    debug)
+        do_debug ${@:2}
+        ;;
+
+    restart|reload|force-reload)
+        do_stop
+        do_start
+        ;;
+
+    status)
+        echo "Haven't implemented status"
+        ;;
+
+    *)
+        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
+        exit 1
+        ;;
+
+esac
+echo rc=$RETVAL
+exit $RETVAL
